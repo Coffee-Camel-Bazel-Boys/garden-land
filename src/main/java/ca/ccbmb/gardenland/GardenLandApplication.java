5ee811c266034c8ec20e29697c73b10259ae78bb
Nickknack <nickknack17@gmail.com>
2023-03-24 20:02:58 -0600

    springboot init

---
+++ src/main/java/ca/ccbmb/gardenland/GardenLandApplication.java
@@ -0,0 +1,13 @@
+package ca.ccbmb.gardenland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenLandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/ccbmb/gardenland/GardenLandApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ccbmb.gardenland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenLandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
