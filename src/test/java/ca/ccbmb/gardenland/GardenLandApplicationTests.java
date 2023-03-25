c17080fa7cdab13a58efb96b683be15fa7440cee
Nickknack <nickknack17@gmail.com>
2023-03-24 20:24:59 -0600

    user table create in liquibase

--- src/test/java/ca/ccbmb/gardenland/GardenLandApplicationTests.java
+++ src/test/java/ca/ccbmb/gardenland/GardenLandApplicationTests.java
@@ -2,9 +2,12 @@ package ca.ccbmb.gardenland;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class GardenLandApplicationTests {
+@ActiveProfiles("test")
+class  GardenLandApplicationTests {
 
     @Test
     void contextLoads() {
